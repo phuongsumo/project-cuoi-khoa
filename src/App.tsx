b171ae8e1be278396367bc37ba7e19364a957598
Thanh Dat <ngu.tki.sao99@gmail.com>
2022-03-21 17:24:06 +0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        
       </Routes>
+      <Footer/>
     </div>
   );
 }
