73f4d4fa0173481dfb20d95026f4ea4a60c7ef99
unknown <anhthunguyen12700@gmail.com>
2022-04-01 19:54:23 +0700

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,9 @@ export { default as Footer } from "./footer/Footer";
 export { default as Product } from "./product/Product";
 export { default as HistoryAndMission } from "./historyAndMission/HistoryAndMission";
 export { default as Achievement } from "./achievement/Achievement";
+export { default as CreateAccount}from './createAccount/CreateAccount'
+export { default as Login }from './login/Login'
+export { default as News }from './news/News'
+export { default as Commercial_story }from './news/bodyNews/commercial_story/Commercial_story'
+export { default as Promotional_news }from './news/bodyNews/promotional news/Promotional_news'
+export { default as Sk_events }from './news/bodyNews/sk_events/Sk_events'
